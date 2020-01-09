980379cbff0e4311201ddec28e1f6397e91b1d97
Pierre MINIGGIO <=>
2020-01-09 14:34:30 +0100

    multi likes

--- like_button.js
+++ like_button.js
@@ -1,25 +1,36 @@
-'use strict';
-
-const e = React.createElement;
+const e = React.createElement
 
 class LikeButton extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = { liked: false };
+    super(props)
+    this.state = { liked: 0 }
   }
 
   render() {
+
+    let html = ''
+
     if (this.state.liked) {
-      return 'You liked this.';
+      html += this.state.liked + ' '
+    }
+
+    html += 'Like'
+
+    if (this.state.liked > 1) {
+      html += 's'
     }
 
     return e(
       'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
+      {
+        onClick: () => {
+          this.setState({ liked: this.state.liked + 1 })
+        }
+      },
+      html
+    )
   }
 }
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+const domContainer = document.querySelector('#like_button_container')
+ReactDOM.render(e(LikeButton), domContainer)
